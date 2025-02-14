dc2bc5135947ca0bff9ff5e480ff9eded0dade1b
salmanpatnee <salmanpatni92@gmail.com>
2025-02-14 12:12:41 +0500

    Setup vue router and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello Vue</h1>
+  <router-view />
 </template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
