5fe3dd6af8c63ce99d21dad406069189495af156
salmanpatnee <salmanpatni92@gmail.com>
2025-02-19 14:05:48 +0500

    Add bulk seed entries

--- database/seed.js
+++ database/seed.js
@@ -4,15 +4,21 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
-  try {
+const seedProjects = async (numProjects = 10) => {
+  const projects = []
+
+  for (let i = 0; i < numProjects; i++) {
     const name = faker.lorem.words(3)
-    const { error } = await supabase.from('projects').insert({
+    projects.push({
       name,
       slug: name.toLowerCase().replace(/ /g, '-'),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       collaborators: faker.helpers.arrayElements([1, 2, 3, 4]),
     })
+  }
+
+  try {
+    const { error } = await supabase.from('projects').insert(projects)
 
     if (error) {
       throw error
@@ -23,4 +29,4 @@ const seedProjects = async () => {
   }
 }
 
-await seedProjects()
+await seedProjects(50)
