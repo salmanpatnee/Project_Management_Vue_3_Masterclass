8fb85618e81264acff99f54b899c00c5439db2a8
salmanpatnee <salmanpatni92@gmail.com>
2025-02-19 13:05:32 +0500

    Set up Supabase client in node.

--- database/seed.js
+++ database/seed.js
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
 
-// const randomName = faker.person.fullName()
-// const randomEmail = faker.internet.email()
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
