7ed97b64429e782b361a74fd9631c58fe220ef36
salmanpatnee <salmanpatni92@gmail.com>
2025-02-24 16:59:30 +0500

    Create sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,14 @@
 <script setup lang="ts">
-import { Input } from '@/components/ui/input'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import TopNav from '@/components/layout/TopNav.vue'
+import AppSidebar from '@/components/layout/AppSidebar.vue'
 </script>
 
 <template>
-  <nav class="flex justify-between items-center h-16 border-b bg-muted/40 px-6">
-    <form class="w-full max-w-96 relative h-fit">
-      <iconify-icon
-        icon="lucide:search"
-        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
-      ></iconify-icon>
-      <Input type="search" class="w-full pl-8 bg-background" placeholder="Search..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <router-view />
+  <AppSidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNav />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
