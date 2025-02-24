74a41849a458ab54baf651fab03cb29325d75f8b
salmanpatnee <salmanpatni92@gmail.com>
2025-02-24 16:01:17 +0500

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
+  <nav class="flex justify-between items-center h-16 border-b bg-muted/40 px-6">
+    <form class="w-full max-w-96">
+      <input type="search" class="w-full pl-8 bg-background" placeholder="Search..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <router-view />
 </template>
