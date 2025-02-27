4a52572829be39eb842b0473207e4dfd5aa442fc
salmanpatnee <salmanpatni92@gmail.com>
2025-02-27 14:12:26 +0500

    Add basic datatable example

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, h } from 'vue'
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/datatable.vue'
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+]
+const data = ref<Payment[]>([])
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 
@@ -11,14 +37,30 @@ const fetchTasks = async () => {
   tasks.value = data
 }
 
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    // ...
+  ]
+}
+
 onMounted(async () => {
   await fetchTasks()
+  data.value = await getData()
 })
 </script>
 
 <template>
   <div>
     <h1>Tasks</h1>
+    <DataTable :columns="columns" :data="data" />
+
     <ul>
       <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
     </ul>
